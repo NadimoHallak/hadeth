f6fc17833e0637cd8024609c927f0157586bffee
Nadim <workgith@gmail.com>
2024-03-02 14:11:29 -0800

    final logic

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -33,84 +33,91 @@ class _ResultPageState extends State<ResultPage> {
         centerTitle: true,
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundColor: const Color.fromARGB(255, 253, 82, 82),
-                        child: Text(
-                          trueCount == 0
-                              ? widget.data[0].length.toString()
-                              : trueCount != widget.data[0].length
-                                  ? (widget.data[0].length - trueCount)
-                                      .toString()
-                                  : errorCount.toString(),
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 25),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundColor:
+                              const Color.fromARGB(255, 253, 82, 82),
+                          child: Text(
+                            trueCount == 0
+                                ? widget.data[0].length.toString()
+                                : trueCount != widget.data[0].length
+                                    ? (widget.data[0].length - trueCount)
+                                        .toString()
+                                    : errorCount.toString(),
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 25),
+                          ),
                         ),
-                      ),
-                      const Text(
-                        "عدد الأخطاء",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
+                        const Text(
+                          "عدد الأخطاء",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      )
-                    ],
-                  ),
-                ],
+                        const SizedBox(
+                          height: 50,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Text(
-              errorCount > 0 ? "{مكان تواجد الأخطاء}" : "{لا يوجد أخطاء}",
-              style: const TextStyle(
-                color: Colors.deepPurpleAccent,
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Wrap(
-                textDirection: TextDirection.rtl,
-                children: correctErrorText,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            if (errorCount > 0)
-              const Text(
-                "{تصحيح الأخطاء}",
-                style: TextStyle(
+              Text(
+                errorCount > 0 ? "{مكان تواجد الأخطاء}" : "{لا يوجد أخطاء}",
+                style: const TextStyle(
                   color: Colors.deepPurpleAccent,
                   fontSize: 20,
                 ),
               ),
-            if (errorCount > 0)
               const SizedBox(
                 height: 15,
               ),
-            if (errorCount > 0)
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Wrap(
                   textDirection: TextDirection.rtl,
-                  children: correctText,
+                  children: correctErrorText,
                 ),
               ),
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              if (errorCount > 0)
+                const Text(
+                  "{تصحيح الأخطاء}",
+                  style: TextStyle(
+                    color: Colors.deepPurpleAccent,
+                    fontSize: 20,
+                  ),
+                ),
+              if (errorCount > 0)
+                const SizedBox(
+                  height: 15,
+                ),
+              if (errorCount > 0)
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Wrap(
+                    textDirection: TextDirection.rtl,
+                    children: correctText,
+                  ),
+                ),
+              if (errorCount > 0)
+                const SizedBox(
+                  height: 30,
+                )
+            ],
+          ),
         ),
       ),
     );
@@ -334,27 +341,145 @@ class _ResultPageState extends State<ResultPage> {
             ),
           );
         } else {
-          errorCount++;
-          correctErrorText.add(
-            Text(
-              "${widget.wordSpoken[i]} ",
-              style: TextStyle(
-                color: errorColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+          if (widget.wordSpoken[i] == widget.data[0][i + 1] &&
+              i + 1 < widget.wordSpoken.length) {
+            errorCount += 2;
+            correctErrorText.add(
+              Text(
+                ".....",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
               ),
-            ),
-          );
-          correctText.add(
-            Text(
-              "${widget.data[0][i]} ",
-              style: TextStyle(
-                color: correctColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+            );
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: trueColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
               ),
-            ),
-          );
+            );
+            correctText.add(
+              Text(
+                "${widget.data[0][i]} ",
+                style: TextStyle(
+                  color: correctColor,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          } else if (widget.wordSpoken[i] == widget.data[0][i + 2] &&
+              i + 2 < widget.wordSpoken.length) {
+            errorCount += 3;
+            correctErrorText.add(
+              Text(
+                "..........",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: trueColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctText.add(
+              Text(
+                "${widget.data[0][i]} ",
+                style: TextStyle(
+                  color: correctColor,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          } else if (widget.wordSpoken[i] == widget.data[0][i - 1] && i > 0) {
+            errorCount += 2;
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: trueColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctErrorText.add(
+              Text(
+                ".....",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctText.add(
+              Text(
+                "${widget.data[0][i]} ",
+                style: TextStyle(
+                  color: correctColor,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          } else if (widget.wordSpoken[i] == widget.data[0][i - 2] && i > 1) {
+            errorCount += 3;
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: trueColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctErrorText.add(
+              Text(
+                "..........",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          } else {
+            errorCount++;
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctText.add(
+              Text(
+                "${widget.data[0][i]} ",
+                style: TextStyle(
+                  color: correctColor,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          }
         }
       }
     } else {
@@ -381,27 +506,145 @@ class _ResultPageState extends State<ResultPage> {
             ),
           );
         } else {
-          errorCount++;
-          correctErrorText.add(
-            Text(
-              "${widget.wordSpoken[i]} ",
-              style: TextStyle(
-                color: errorColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+          if (widget.wordSpoken[i] == widget.data[0][i + 1] &&
+              i + 1 < widget.wordSpoken.length) {
+            errorCount += 2;
+            correctErrorText.add(
+              Text(
+                ".....",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
               ),
-            ),
-          );
-          correctText.add(
-            Text(
-              "${widget.data[0][i]} ",
-              style: TextStyle(
-                color: correctColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+            );
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: trueColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
               ),
-            ),
-          );
+            );
+            correctText.add(
+              Text(
+                "${widget.data[0][i]} ",
+                style: TextStyle(
+                  color: correctColor,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          } else if (widget.wordSpoken[i] == widget.data[0][i + 2] &&
+              i + 2 < widget.wordSpoken.length) {
+            errorCount += 3;
+            correctErrorText.add(
+              Text(
+                "..........",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: trueColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctText.add(
+              Text(
+                "${widget.data[0][i]} ",
+                style: TextStyle(
+                  color: correctColor,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          } else if (widget.wordSpoken[i] == widget.data[0][i - 1] && i > 0) {
+            errorCount += 2;
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: trueColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctErrorText.add(
+              Text(
+                ".....",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctText.add(
+              Text(
+                "${widget.data[0][i]} ",
+                style: TextStyle(
+                  color: correctColor,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          } else if (widget.wordSpoken[i] == widget.data[0][i - 2] && i > 1) {
+            errorCount += 3;
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: trueColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctErrorText.add(
+              Text(
+                "..........",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          } else {
+            errorCount++;
+            correctErrorText.add(
+              Text(
+                "${widget.wordSpoken[i]} ",
+                style: TextStyle(
+                  color: errorColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            );
+            correctText.add(
+              Text(
+                "${widget.data[0][i]} ",
+                style: TextStyle(
+                  color: correctColor,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          }
         }
       }
     }
